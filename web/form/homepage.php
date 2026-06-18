98c8bf66038c9b465934d8bd56867de961b4e10c
MIRZA-KAYANG <mirzasyraf@gmail.com>
2026-06-18 11:56:13 +0700

    UPDATE - HOME PAGE

--- web/form/homepage.php
+++ web/form/homepage.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Ombak-Biru (Indonesia)</title>
+    <link rel="icon" type="image/svg+xml" href="../img/Asset 10.svg">
 </head>
 <body>
-    
+    <form>
+        
+    </form>
 </body>
 </html>
