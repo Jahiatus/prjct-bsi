b2e89d68ff3f5eed30629bc95d410981ed3cccd6
MIRZA-KAYANG <mirzasyraf@gmail.com>
2026-06-21 13:16:35 +0700

    UPDATE LOGIN

--- web/form/login.php
+++ web/form/login.php
@@ -1,64 +1,40 @@
 <?php
-
 session_start();
-
 include "config/koneksi.php";
-
-
-if(isset($_POST['login'])){
-
-
-$email=$_POST['email'];
-
-$password=$_POST['password'];
-
-
-$data=mysqli_query($conn,
-
-"SELECT * FROM users 
-WHERE email='$email'");
-
-
-$user=mysqli_fetch_assoc($data);
-
-
-
-if(password_verify(
-$password,
-$user['password']
-)){
-
-
-$_SESSION['user']=$user['id'];
-
-header(
-"location:dashboard.php"
-);
-
-
-}else{
-
-echo "Login gagal";
-
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $data = mysqli_query(
+        $conn,
+        "SELECT * FROM users 
+WHERE email='$email'"
+    );
+    $user = mysqli_fetch_assoc($data);
+    if (
+        password_verify(
+            $password,
+            $user['password']
+        )
+    ) {
+        $_SESSION['user'] = $user['id'];
+        header(
+            "location:dashboard.php"
+        );
+    } else {
+        echo "Login gagal";
+    }
 }
-
-
-}
-
 ?>
 
 <form method="post">
 
-<input name="email" placeholder="Email">
+    <input name="email" placeholder="Email">
 
-<input 
-type="password"
-name="password"
-placeholder="Password">
+    <input type="password" name="password" placeholder="Password">
 
 
-<button name="login">
-Masuk
-</button>
+    <button name="login">
+        Masuk
+    </button>
 
 </form>
