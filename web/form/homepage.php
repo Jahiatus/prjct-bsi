c0a60614bad1be4f082ee9a127d5e460227cf4b2
MIRZA-KAYANG <mirzasyraf@gmail.com>
2026-06-23 16:03:34 +0700

    UPDATE HOMEPAGE

--- web/form/homepage.php
+++ web/form/homepage.php
@@ -6,6 +6,9 @@
     <title>Ombak-Biru (Indonesia)</title>
     <link rel="icon" type="image/svg+xml" href="../img/Asset 10.svg">
     <link rel="stylesheet" href="../css/test.css">
+    <link rel="stylesheet" href="../css/bdy.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
 </head>
 <body>
     <?php include '../layout/hdr.html'; ?>
