dc475510812f47c6ee8620f1a684e03c1a010a72
MIRZA-KAYANG <mirzasyraf@gmail.com>
2026-06-23 16:23:48 +0700

    UPDATE HOMEPAGE

--- web/form/homepage.php
+++ web/form/homepage.php
@@ -5,10 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ombak Biru - Solusi Transportasi Bahari Anda</title>
     <link rel="icon" type="image/svg+xml" href="../img/Asset 10.svg">
-    <link rel="stylesheet" href="../css/test.css">
-    <link rel="stylesheet" href="../css/bdy.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
 </head>
 <body>
     <?php include '../layout/hdr.html'; ?>
