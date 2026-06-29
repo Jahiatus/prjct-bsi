758f534cc5f151d2832f64082a592674a994ec67
abyanassyaddad <abyanassyaddad@gmail.com>
2026-06-29 15:56:39 +0700

    abyan

--- web/form/hubungi_kami.php
+++ web/form/hubungi_kami.php
@@ -12,7 +12,120 @@
 <?php include __DIR__ . '/../layout/hdr.html'; ?>
 
 <main>
-    <?php include __DIR__ . '/../layout/komen.html'; ?>
+<body>
+    
+<link rel="stylesheet" href="../css/form.css" />
+    <section id="contact-us">
+      <div class="card">
+        <h2 class="card-title">Hubungi Kami</h2>
+
+        <p>
+          Tinggalkan pesan kepada kami. Tim Ombak Biru akan segera menghubungi
+          Anda.
+        </p>
+
+        <form
+          class="booking-form"
+          id="form"
+          name="form"
+          method="POST"
+          action="https://forms.zohopublic.com/contacus-form/form/WanttoTalkForm/formperma/TzNwbTAd3xQ06hsBJnZeSWdOiNTw53_Hjh6qOpuzggs/htmlRecords/submit"
+          accept-charset="UTF-8"
+          enctype="multipart/form-data"
+          onsubmit='javascript:document.charset="UTF-8"; return zf_ValidateAndSubmit();'
+        >
+          <input type="hidden" name="zf_referrer_name" value="" />
+          <input type="hidden" name="zf_redirect_url" value="" />
+          <input type="hidden" name="zc_gad" value="" />
+
+          <div class="form-group">
+            <label>Nama Depan</label>
+
+            <div class="input-wrapper">
+              <input
+                type="text"
+                name="Name_First"
+                maxlength="255"
+                fieldType="7"
+              />
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label>Nama Belakang</label>
+
+            <div class="input-wrapper">
+              <input
+                type="text"
+                name="Name_Last"
+                maxlength="255"
+                fieldType="7"
+              />
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label>Email</label>
+
+            <div class="input-wrapper">
+              <input type="email" name="Email" maxlength="255" fieldType="9" />
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label>Nomor Telepon</label>
+
+            <div class="input-wrapper">
+              <input
+                type="text"
+                name="PhoneNumber_countrycode"
+                maxlength="20"
+                fieldType="11"
+              />
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label>Hubungi Kami Melalui</label>
+
+            <div class="input-wrapper">
+              <label>
+                <input type="radio" name="Radio" value="Phone" />
+                Phone
+              </label>
+
+              <label>
+                <input type="radio" name="Radio" value="Email" />
+                Email
+              </label>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label>Pertanyaan</label>
+
+            <div class="input-wrapper">
+              <select name="Dropdown">
+                <option value="-Select-">-Select-</option>
+                <option value="Sales">Sales</option>
+                <option value="Service">Service</option>
+              </select>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label>Masukan Pesan Anda</label>
+
+            <div class="input-wrapper">
+              <textarea name="MultiLine" maxlength="65535"> </textarea>
+            </div>
+          </div>
+
+          <button class="btn-submit" type="submit">Kirim Pesan</button>
+        </form>
+      </div>
+    </section>
+  </body>
 </main>
 
 <?php include __DIR__ . '/../layout/ftr.html'; ?>
