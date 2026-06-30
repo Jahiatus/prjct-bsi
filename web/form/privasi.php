1c84f58d6a6713b5bcd1c6fc5b143f8b368fbbaf
abyanassyaddad <abyanassyaddad@gmail.com>
2026-06-30 16:19:47 +0700

    iciiwir

---
+++ web/form/privasi.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ombak Biru - Solusi Transportasi Bahari Anda</title>
+    <link rel="icon" type="image/svg+xml" href="../img/Asset 10.svg">
+</head>
+
+<body>
+
+<?php include __DIR__ . '/../layout/hdr.html'; ?>
+
+<main>
+    <?php include __DIR__ . '/../layout/privasi.html'; ?>
+</main>
+
+<?php include __DIR__ . '/../layout/ftr.html'; ?>
+</body>
+</html>
