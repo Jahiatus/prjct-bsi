dc41aa88d9ec620b67d1fb2334768ce4bcb956ee
Satrio Wibowo <satriowibowo100806@gmail.com>
2026-07-03 10:18:03 +0700

    update nich

--- web/form/partner.php
+++ web/form/partner.php
@@ -2,20 +2,19 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Partner Portal | Ombak Biru</title>
 
     <link rel="icon" href="../img/Asset 10.svg">
-    <link rel="stylesheet" href="../css/partner_portal.css">
+    <link rel="stylesheet" href="../css/partner.css">
 </head>
 
 <body>
 
-<?php include __DIR__.'/../layout/hdr.html'; ?>
+<?php include __DIR__ . "/../layout/hdr.html"; ?>
 
-<?php include __DIR__.'/../layout/partner_portal.html'; ?>
+<?php include __DIR__ . "/../layout/partner.html"; ?>
 
-<?php include __DIR__.'/../layout/ftr.html'; ?>
+<?php include __DIR__ . "/../layout/ftr.html"; ?>
 
 </body>
 </html>
