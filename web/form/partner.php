25a05aa4973626c0080de87303861820acae62a9
Satrio Wibowo <satriowibowo100806@gmail.com>
2026-07-03 09:21:25 +0700

    gaskeun

---
+++ web/form/partner.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Partner Portal | Ombak Biru</title>
+
+    <link rel="icon" href="../img/Asset 10.svg">
+    <link rel="stylesheet" href="../css/partner_portal.css">
+</head>
+
+<body>
+
+<?php include __DIR__.'/../layout/hdr.html'; ?>
+
+<?php include __DIR__.'/../layout/partner_portal.html'; ?>
+
+<?php include __DIR__.'/../layout/ftr.html'; ?>
+
+</body>
+</html>
